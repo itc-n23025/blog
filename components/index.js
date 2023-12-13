0bdded07cc3889629a6cb15205e420369d3dabba
itc-n23025 <n23025@std.it-college.ac.jp>
2023-12-13 07:56:15 +0000

    add 3 components
    3つのconponentsを追加

---
+++ components/index.js
@@ -0,0 +1,17 @@
+import Header from '../components/header'
+import Hero from '../components/hero'
+import Footer from '../ponents/footer'
+
+export default function Home () {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <Hero />
+      </main>
+
+      <Footer />
+    </>
+  )
+}
