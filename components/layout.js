f464d86f966224ca54be28b2e652715425be0e8e
itc-n23025 <n23025@std.it-college.ac.jp>
2023-12-15 04:00:14 +0000

    last edit

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,6 @@
 import Header from 'components/header'
 import Footer from 'components/footer'
-const Layout = children => {
+const Layout = ({ children }) => {
   return (
     <>
       <Header />
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Layout from 'components/layout'
+import '@/styles/globals.css'
 const MyApp = ({ Component, pageProps }) => {
   return (
     <Layout>
